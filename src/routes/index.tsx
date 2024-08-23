312c2c14a5c881a5190b54d5fd58e6ef2ed4fb19
MaymoonaAlBoloshi <maymoona.balushi@rihal.om>
2024-08-23 12:10:37 +0400

    search and filter ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Card } from 'components/Card'
+import { Card, Search } from 'components'
 
 export const Route = createFileRoute('/')({
-    component: () => {
-        return (
-            <section className="flex flex-wrap gap-4 justify-center items-center">
-                {[1, 2, 3, 4, 1, 2, 3].map((item, index) => (
-                    <Card key={index} />
-                ))}
-            </section>
-        )
-    }
+  component: () => {
+    return (
+      <section className="flex flex-col justify-center items-center gap-4">
+        <Search />
+        <section className="flex flex-wrap gap-4 justify-center items-center">
+          {[1, 2, 3, 4, 1, 2, 3].map((item, index) => (
+            <Card key={index} />
+          ))}
+        </section>
+      </section>
+    )
+  }
 })
