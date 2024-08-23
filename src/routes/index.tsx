6100f827bcaeb2f9779a7c27ddb4f5ea55a7f365
Maymoona Al Balushi <maymoona.balushi@rihal.om>
2024-08-23 11:59:48 +0400

    base card list (#2)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Card } from 'components/Card'
 
 export const Route = createFileRoute('/')({
-  component: () => <div>Hello /!</div>
+    component: () => {
+        return (
+            <section className="flex flex-wrap gap-4 justify-center items-center">
+                {[1, 2, 3, 4, 1, 2, 3].map((item, index) => (
+                    <Card key={index} />
+                ))}
+            </section>
+        )
+    }
 })
